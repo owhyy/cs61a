26501fe30409f4e64a32b051acf6dce86b389503
Babin Ion <ionbabin123@mail.ru>
2022-03-03 12:06:23 +0200

    removed single files from homework, added them all in one

--- week1/homework/squares.scm
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require berkeley)
-
-(define (square x) (* x x))
-
-(define (squares sent)
-  (if (empty? sent) empty
-      (se (square (first sent))
-          (squares (bf sent)))))
