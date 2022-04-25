8bd84685f1b0c90c394c7219b49a1cbd624e6fbf
Babin Ion <ionbabin123@mail.ru>
2022-04-25 13:48:08 +0300

    finished 2_37

--- week5/sicp/2_37.scm
+++ week5/sicp/2_37.scm
@@ -1,36 +1,51 @@
 #lang racket
+(require rackunit)
 
 (define (accumulate p init seq)
   (if (empty? seq)
-      init
-      (p (car seq)
-         (accumulate p init (cdr seq)))))
+    init
+    (p (car seq)
+       (accumulate p init (cdr seq)))))
 
 (define (accumulate-n p init seqs)
   (if (empty? (car seqs))
-      init
-      (cons (accumulate p init (map car seqs))
-            (accumulate-n p init (map cdr seqs)))))
+    null
+    (cons (accumulate p init (map car seqs))
+          (accumulate-n p init (map cdr seqs)))))
 
 (define (dot-product v w)
   (accumulate + 0 (map * v w)))
 
 (define (matrix-*-vector m v)
-  (map (lambda (x) (map (lambda (y) (* v y)) x)) m))
+  (map (lambda (w)
+         (dot-product w v)) m))
 
 (define (transpose mat)
   (accumulate-n cons empty mat))
 
-;;  ???
 (define (matrix-*-matrix m n)
   (let ((cols (transpose n)))
-    (map m)))
-(define v1 '((1 2 3 4) (4 5 6 6)))
+    (map (lambda (x) (matrix-*-vector cols x)) m)))
 
-; 1*1 + 2*2 + 3*3 + 4*4 = 30
-(dot-product (car v1) (car v1))
+(define matrix '((1 2 3 4) (4 5 6 6)))
+(define tests
+  (test-suite
+    "Tests for excersie 2.37"
 
-; new matrix, everything is multiplied by a vector
-(matrix-*-vector v1 3)
-(transpose v1)
-(matrix-*-matrix v1 v1)
+    ; accumulate test
+    (check-equal? (accumulate + 0 '(1 2 3 4)) 10 "accumulate test")
+    ; accumulate-n test
+    (check-equal? (accumulate-n + 0 '((1 2 3 4) (0 0 0 0))) '(1 2 3 4) "accumulate-n test")
+    ; dot-product test
+    (check-equal? (dot-product (car matrix) (car matrix)) 30 "dot-product test")
+    ; matrix-*-vector test
+    (check-equal? (matrix-*-vector matrix '(1 1 1 1)) '(10 21) "matrix-*-vector test")
+    ; transpose test
+    (check-equal? (transpose matrix) '((1 4) (2 5) (3 6) (4 6)) "transpose test")
+    ; matrix-*-matrix test
+    (check-equal? (matrix-*-matrix matrix '((1 1) (1 1) (1 1) (1 1))) '((10 10) (21 21)) "matrix-*-matrix test")
+    ))
+
+(require rackunit/text-ui)
+;; runs the test
+(run-tests tests)
